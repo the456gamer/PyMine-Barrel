621c46a86da77a8b5fb1b0a431d80f92bcdf5110
The456gamer <The456gamer@the456gamer.dev>
2021-02-27 21:01:55 +0000

    clear init file from debugging

--- barrel/__init__.py
+++ barrel/__init__.py
@@ -1,8 +1,1 @@
-# run when module is imported
-# ("python -m barrel")
-# ("python barrel/__init__.py")
-print("__init__:base")
-
-if __name__ == "__main__":
-    print("__init__:__name__==__main__")
-    
+# Package File
